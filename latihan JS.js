2cd274c7da36e215e863e88c6eb18eb3eeb5a880
Khairunnisah13 <109327181+khairunnisah13@users.noreply.github.com>
2022-09-15 11:58:05 +0700

    Add files via upload

---
+++ latihan JS.js
@@ -0,0 +1,114 @@
+// // Penulisan tipe data number
+// let umur = 16;
+// // Penulisan tipe data string
+// let data = "statistik";
+// //tipe data boolean
+// let data1 = true;
+// //tipe data null
+// let data2 = null;
+// //tipe data undifined
+// let data3;
+
+// console.log(typeof data1);
+// let data = 12;
+// data--;
+// console.log(data);
+
+// console.log(data1 + data2);
+// console.log(data1 - data2);
+// console.log(data1 * data2);
+// console.log(data1 / data2);
+// console.log(data1 % 3);
+// console.log(data1 != 11);
+
+// console.log(data1 < 10 != data2 >= 20);
+
+// if (data1) {
+//   console.log("true");
+// }
+
+// let warna = "putih";
+
+// if (warna === "yellow") {
+//   console.log("warna terang");
+// } else if (warna === "biru") {
+//   console.log("warna langit");
+// } else if (warna === "hijau") {
+//   console.log("warna sejuk");
+// } else {
+//   console.log("bukan warna");
+// }
+
+// let inputusername = submitData || "nisa";
+
+// if (inputusername) {
+//   console.log("submitData");
+// } else {
+//   console.log("nisa");
+// }
+
+// let menu = 4;
+
+// switch (menu) {
+//   case 1: {
+//     console.log("menu 1");
+//     break;
+//   }
+//   case 2: {
+//     console.log("menu 2");
+//     break;
+//   }
+//   case 3: {
+//     console.log("menu 3");
+//     break;
+//   }
+//   case 4: {
+//     console.log("menu 4");
+//     break;
+//   }
+//   default: {
+//     console.log("bukan menu");
+//     break;
+//   }
+// }
+
+// let isNowSales = false;
+// isNowSales ? console.log("yuk belanja") : console.log("belanja nanti aja");
+
+// let nilai = 10;
+
+// for (nilai = 0; nilai <= 9; nilai++) {
+//   console.log(nilai);
+// }
+
+//
+// const panjang = 20;
+// const lebar = 30;
+// const area = panjang * lebar;
+
+// function area(panjang, lebar){
+//     return panjang* lebar
+// }
+// console.log(area);
+// function greeting() {
+//   console.log("hello world");
+// }
+
+// let panjang = 20;
+// let lebar = 30;
+// luasPersegiPanjang;
+
+// // const luasPersegiPanjang(panjang, lebar) {
+// //     console.log(luasPersegiPanjang(70, 60));}
+
+// console.log(luasPersegiPanjang(20, 30) + luasPersegiPanjang(50, 40));
+// let username() {
+//     nama: "nisa"
+//     alamat: "seketeng"
+// }
+
+// let data1 = [1, 2, 3, "hay",b
+
+// button.getaddEventListener("click");{
+//     alert("Welcome")
+// };
